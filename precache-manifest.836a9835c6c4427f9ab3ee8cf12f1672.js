eb9b37cb739c5da7022d83f1d8e6be402a9405b5
yanyabing1 <457958667@qq.com>
2019-12-26 20:22:04 +0800

    Updates

---
+++ precache-manifest.836a9835c6c4427f9ab3ee8cf12f1672.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e956bae86a78a613fd5c5cb4b7e7ac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "45a27eb2932502a61399",
+    "url": "./static/css/main.7233220b.chunk.css"
+  },
+  {
+    "revision": "74481ad881fcb0d21a1b",
+    "url": "./static/js/2.9660790f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.9660790f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45a27eb2932502a61399",
+    "url": "./static/js/main.d9e7fbb4.chunk.js"
+  },
+  {
+    "revision": "110301cad72a71f88ba7",
+    "url": "./static/js/runtime-main.a52a1fa3.js"
+  }
+]);
