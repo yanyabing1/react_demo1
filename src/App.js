e9c741b6f4c0125ada9297a2fe914bef1965250f
yanyabing1 <457958667@qq.com>
2019-12-26 20:22:47 +0800

    1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Layout from './Layout';
+import './assets/reset.css'
+/* 备注文件 */
+
+function App() {
+  return (
+    <Layout/>
+  );
+}
+
+export default App;
